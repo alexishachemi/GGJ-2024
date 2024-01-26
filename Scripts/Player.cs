548b880c3c156d9696f75ace6653e95146423e6b
Alexis <alexishachemi@gmail.com>
2024-01-26 23:45:32 +0100

    Add animation wonder

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -8,12 +8,14 @@ public partial class Player : CharacterBody2D
 
 	private Vector2 syncPos = new Vector2(0,0);
 	private float syncRotation = 0;
+	private AnimatedSprite2D _animatedSprite;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
 	public override void _Ready(){
 		GetNode<MultiplayerSynchronizer>("MultiplayerSynchronizer").SetMultiplayerAuthority(int.Parse(Name));
+		_animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite");
 	}
 	public override void _PhysicsProcess(double delta)
 	{
@@ -26,19 +28,27 @@ public partial class Player : CharacterBody2D
 				velocity.Y += gravity * (float)delta;
 
 			// Handle Jump.
-			if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+			if (Input.IsActionJustPressed("ui_accept") && IsOnFloor()) {
 				velocity.Y = JumpVelocity;
+			}
+			if (!IsOnFloor())
+				_animatedSprite.Play("jump");
 
 			// Get the input direction and handle the movement/deceleration.
 			// As good practice, you should replace UI actions with custom gameplay actions.
 			Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 			if (direction != Vector2.Zero)
 			{
+				if (IsOnFloor())
+					_animatedSprite.Play("move");
 				velocity.X = direction.X * Speed;
 			}
 			else
 			{
 				velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			}
+			if (velocity.X == 0 && IsOnFloor()) {
+				_animatedSprite.Play("stand");
 			}
 
 			Velocity = velocity;
